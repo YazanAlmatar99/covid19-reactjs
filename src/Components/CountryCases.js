fc78656fd795d6289e6e1f564d9cadbbc39d7955
Yazan <yazanmatar90@gmail.com>
2020-05-04 22:41:14 -0400

    changed the default value of countries

--- src/Components/CountryCases.js
+++ src/Components/CountryCases.js
@@ -6,7 +6,7 @@ class CountryCases extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedCountry: "United States of America",
+      selectedCountry: "afghanistan",
       countries: null,
       countriesFormatted: null,
     };
